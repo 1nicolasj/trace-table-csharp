e42951d8f46f64585ddf05126914f8c2328a32e6
1nicolasj <1nicolas.dasilvaa@gmail.com>
2025-05-27 17:55:08 -0300

    Exercise 2 done

--- TraceTable2/TraceTable2/CalculadoraFutureValue.cs
+++ TraceTable2/TraceTable2/CalculadoraFutureValue.cs
@@ -18,39 +18,40 @@ namespace TraceTable2
         //recebe dados e mostra tabela com rendimento, renda líquida e renda acumulada
         public static void MostrarTabelaRendimento(double valorPresente, double taxaJuros, int periodoMes)
         {
-            double rendLiquido = 0;
-            double rendAcumulada = valorPresente + rendLiquido;
+            double rendimento = valorPresente;
+            double rendaAcumulada = 0;
             Console.WriteLine();
+            Console.WriteLine("Loop a.m.\tTaxa Juros\tRendimento\tRend. Líquido\tRenda Acumulada");
             for (int i = 1; i <= periodoMes; i++)
             {
-                double valorF = CalculadoraFutureValue.CalcularValorFuturo(valorPresente, taxaJuros, i);
-                rendLiquido = valorF - valorPresente;
-                rendAcumulada += rendLiquido;
+                double rendaLiquida = rendimento * (taxaJuros / 100);
+                rendimento += rendaLiquida;
+                rendaAcumulada += rendaLiquida;
 
-                Console.Write($"Mês: {i}\tTx Juros: {taxaJuros}%\tRendimento: R${valorF:F2}\tR. Líquida: R${rendLiquido:F2}\tR. Acumulada: R${rendAcumulada:F2}");
-                Console.WriteLine();
+                Console.WriteLine($"{i}\t\t{taxaJuros:F2}%\t\tR${rendimento:F2}\tR${rendaLiquida:F2}\t\tR${rendaAcumulada:F2}");
             }
         }
 
         //recebe dados e mostra tabela com rendimento, renda líquida, renda acumulada e resgate
         public static void MostrarTabelaRendimentoComResgate(double valorPresente, double taxaJuros, int periodoMes, int mesResgate, double valorResgate)
         {
-            double rendLiquido = 0;
-            double rendAcumulada = valorPresente + rendLiquido;
+            double saldo = valorPresente;
+            double rendaAcumulada = 0;
             Console.WriteLine();
+            Console.WriteLine("Mês\tTaxa Juros\tRendimento\tR. Líquida\tR. Acumulada\tResgate\t\tSaldo Final");
             for (int i = 1; i <= periodoMes; i++)
             {
+                double rendaLiquida = saldo * (taxaJuros / 100);
+                saldo += rendaLiquida;
+                rendaAcumulada += rendaLiquida;
+
+                double resgate = 0;
                 if (i == mesResgate)
                 {
-                    rendAcumulada -= valorResgate;
-                    Console.WriteLine($"\nResgate de R${valorResgate:F2} no mês {i}\n");
+                    resgate = valorResgate;
+                    saldo -= resgate;
                 }
-                double valorF = CalculadoraFutureValue.CalcularValorFuturo(valorPresente, taxaJuros, i);
-                rendLiquido = valorF - valorPresente;
-                rendAcumulada += rendLiquido;
-                Console.Write($"Mês: {i}\tTx Juros: {taxaJuros}%\tRendimento: R${valorF:F2}\tR. Líquida: R${rendLiquido:F2}\tR. Acumulada: R${rendAcumulada:F2}");
-                Console.WriteLine();
-                
+                Console.WriteLine($"{i}\t{taxaJuros:F2}%\t\tR${saldo + resgate:F2}\tR${rendaLiquida:F2}\tR${rendaAcumulada:F2}\t\t{(resgate > 0 ? $"R${resgate:F2}" : "-")}\t\tR${saldo:F2}"); //ternario para verificar resgate
             }
         }
 
